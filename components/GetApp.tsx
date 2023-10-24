ba4739889c9bcc7f9b2baf7e5bdfe8988b5d1bb8
datanabiyev <121968335+datanabiyev@users.noreply.github.com>
2023-10-24 17:18:23 +0600

    Update GetApp.tsx

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -7,7 +7,7 @@ const GetApp = () => {
     <section className="flexCenter w-full flex-col pb-[100px]">
       <div className="get-app">
         <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
-          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Get for free now!</h2>
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Apply to NU now!</h2>
           <p className="regular-16 text-gray-10">Available on iOS and Android</p>
           <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
             <Button 
