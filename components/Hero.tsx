f5e87c33a92a1e26e64f5d5f39e0bff0ec5cdf4e
datanabiyev <121968335+datanabiyev@users.noreply.github.com>
2023-10-24 17:24:48 +0600

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,24 +8,24 @@ const Hero = () => {
 
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
         <Image 
-          src="/camp.svg"
-          alt="camp"
+          src="/campus.svg"
+          alt="campus"
           width={50}
           height={50}
           className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         />
-        <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
+        <h1 className="bold-52 lg:bold-88">NU facilities</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
-          We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app
+          Want to get in shape with the help of otehr stduents attending NU? Don't worry, we got you!
         </p>
 
         <div className="my-11 flex flex-wrap gap-5">
           <div className="flex items-center gap-2">
             {Array(5).fill(1).map((_, index) => (
               <Image 
-                src="/star.svg"
+                src="/sports.svg"
                 key={index}
-                alt="star"
+                alt="sports"
                 width={24}
                 height={24}
               />
@@ -41,13 +41,13 @@ const Hero = () => {
         <div className="flex flex-col w-full gap-3 sm:flex-row">
           <Button 
             type="button" 
-            title="Download App" 
+            title="Open Website" 
             variant="btn_green" 
           />
           <Button 
             type="button" 
             title="How we work?" 
-            icon="/play.svg"
+            icon="/competitions.svg"
             variant="btn_white_text" 
           />
         </div>
@@ -61,7 +61,7 @@ const Hero = () => {
               <p className="regular-16 text-gray-20">Location</p>
               <Image src="/close.svg" alt="close" width={24} height={24} />
             </div>
-            <p className="bold-20 text-white">Aguas Calientes</p>
+            <p className="bold-20 text-white">Aigerim Daurenbayev</p>
           </div>
 
           <div className="flexBetween">
