c75ce160669ff19428c9b9c050b902798e62149f
datanabiyev <121968335+datanabiyev@users.noreply.github.com>
2023-10-24 17:21:48 +0600

    Update Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -5,20 +5,20 @@ const Guide = () => {
   return (
     <section className="flexCenter flex-col">
       <div className="padding-container max-container w-full pb-24">
-        <Image src="/camp.svg" alt="camp" width={50} height={50} />
+        <Image src="/campus.svg" alt="camp" width={50} height={50} />
         <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
           We are here for you
         </p>
         <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
-          <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">Guide You to Easy Path</h2>
-          <p className="regular-16 text-gray-30 xl:max-w-[520px]">Only with the hilink application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. Invite your friends, relatives and friends to have fun in the wilderness through the valley and reach the top of the mountain</p>
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">Let us guide you to the nu FACILITIES!</h2>
+          <p className="regular-16 text-gray-30 xl:max-w-[520px]">With the help of our app you can dive into the shrining environment of NU and get to see the wonders of NU from within.</p>
         </div>
       </div>
 
       <div className="flexCenter max-container relative w-full">
         <Image 
-          src="/boat.png"
-          alt="boat"
+          src="/pyramid.png"
+          alt="pyramid"
           width={1440}
           height={580}
           className="w-full object-cover object-center 2xl:rounded-5xl"
@@ -38,12 +38,12 @@ const Guide = () => {
                 <p className="regular-16 text-gray-20">Destination</p>
                 <p className="bold-16 text-green-50">48 min</p>
               </div>
-              <p className="bold-20 mt-2">Aguas Calientes</p>
+              <p className="bold-20 mt-2">Aigerim Daurenbayeva</p>
             </div>
 
             <div className='flex w-full flex-col'>
-              <p className="regular-16 text-gray-20">Start track</p>
-              <h4 className="bold-20 mt-2 whitespace-nowrap">Wonorejo Pasuruan</h4>
+              <p className="regular-16 text-gray-20">Start exploring</p>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">Asset Berdibek</h4>
             </div>
           </div>
         </div>
